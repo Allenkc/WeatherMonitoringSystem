4c71d397856410c9cee79b3fab8eee13a7ded434
Allenkc <aa87220@gmail.com>
2020-09-10 21:02:24 +0800

    [add] 完成基本case

--- src/DataBase.java
+++ src/DataBase.java
@@ -23,4 +23,45 @@ public class DataBase {
         this.humidityList.add(humid);
         this.pressureList.add(press);
     }
+
+    public void save(WeatherData weatherData){
+        this.temperatureList.add(weatherData.getTemperature());
+        this.humidityList.add(weatherData.getHumidity());
+        this.pressureList.add(weatherData.getPressure());
+    }
+
+    /**
+     * 輸出資料
+     * ex:
+     * Temperature 21.0
+     * Humidity 0.9
+     * Pressure 1014.5
+     * Temperature 21.0 19.5
+     * Humidity 0.9 0.3
+     * Pressure 1014.5 1015.0
+     * Temperature 21.0 19.5 18.0 17.3
+     * Humidity 0.9 0.3 0.1 0.6
+     * Pressure 1014.5 1015.0 1014.5 1015.0
+     */
+    public void print(){
+        System.out.print("Temperature ");
+        for (double temp :this.temperatureList){
+            System.out.print(temp+" ");
+        }
+        System.out.println();
+        System.out.print("Humidity ");
+        for (double temp :this.humidityList){
+            System.out.print(temp+" ");
+        }
+        System.out.println();
+        System.out.print("Pressure ");
+        for (double temp :this.pressureList){
+            System.out.print(temp+" ");
+        }
+        System.out.println();
+    }
+
+    public boolean isEmpty(){
+        return temperatureList.isEmpty() && humidityList.isEmpty() && pressureList.isEmpty();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class Main {
@@ -22,7 +23,8 @@ public class Main {
         while (line != null) {
             String[] row = line.split("\\s+");
             //TODO 拿 row[0]來判斷指令類型
-            System.out.println(line);
+            checkCmdType(row);
+
             line = reader.readLine();
         }
 
@@ -30,25 +32,100 @@ public class Main {
 
     /**
      * 檢查command Type 並依據指令去做對應的事情
+     *
      * @param row
      * @return
      */
-    private Optional<CommandType> checkCmdType(String[] row) {
+    private static void checkCmdType(String[] row) {
 
-        CommandType cmdType = CommandType.values()[CommandType.toCommandType(row[0]).ordinal()];
+        CommandType cmdType = CommandType.toCommandType(row[0]);
 
         switch (cmdType) {
             case DATA:
-                //TODO 依據內容儲存資料
+                //依據內容儲存資料
+                updateWeatherData(row);
+                //隨後進行輸出
+                output();
                 break;
             case ATTACH:
+                weatherData.setArea(Area.toArea(row[1]));
+                weatherData.setDisplayType(DisplayType.toDisplayType(row[2]));
                 break;
             case DETACH:
+                Area area = Area.toArea(row[1]);
+                if (null != weatherData.getArea() && area.equals(weatherData.getArea())) {
+
+                    DisplayType displayType =
+                            DisplayType.toDisplayType(row[2]);
+                    if (null != weatherData.getDisplayType() &&
+                            displayType.equals(weatherData.getDisplayType())) {
+                        weatherData.setArea(null);
+                        weatherData.setDisplayType(null);
+                    }
+
+                }
+
                 break;
-            default:
-                return null;
+
         }
 
-        return null;
+    }
+
+    /**
+     * 及時更新當下的 weather data obj
+     *
+     * @param data
+     */
+    private static void updateWeatherData(String[] data) {
+
+        Area area = Area.toArea(data[1]);
+
+        weatherData.setTemperature(parseAndRoundDouble(data[2]));
+        weatherData.setHumidity(parseAndRoundDouble(data[3]));
+        weatherData.setPressure(parseAndRoundDouble(data[4]));
+
+        if (area.equals(Area.US)) {
+            // 寫 US DB
+            usDB.save(weatherData);
+        } else {
+            // 寫 ASIA DB
+            asiaDB.save(weatherData);
+        }
+
+    }
+
+    /**
+     * 收到DATA指令的當下要去進行最終輸出
+     */
+    private static void output() {
+        if (null != weatherData.getArea()) {
+            if (Area.US.equals(weatherData.getArea())) {
+                weatherData.print(usDB);
+            } else {
+                weatherData.print(asiaDB);
+            }
+        }
+    }
+
+    /**
+     * parse str to double
+     *
+     * @param str
+     * @return
+     */
+    private static double parseAndRoundDouble(String str) {
+        return round(Double.parseDouble(str), 1);
+    }
+
+    /**
+     * round to one decimal
+     *
+     * @param value
+     * @param precision
+     * @return
+     */
+    private static double round(double value, int precision) {
+        int scale = (int) Math.pow(10, precision);
+        return (double) Math.round(value * scale) / scale;
     }
 }
